c0fc4fe6e0780d94a0deb89e0d932e1ce13934d5
Artur Muller <me@arturmuller.com>
2018-03-15 15:27:47 -0300

    Set 'content.json' as default path for all commands

--- cli/index.js
+++ cli/index.js
@@ -15,7 +15,7 @@ cli
   .option("--project-id <value>", "PhraseApp project ID", null, process.env.PHRASEAPP_PROJECT_ID)
   .option("--access-token <value>", "PhraseApp access token", null, process.env.PHRASEAPP_ACCESS_TOKEN)
   .option("--default-locale <value>", "Default locale code to which empty keys should fall back to", null, process.env.PHRASEAPP_DEFAULT_LOCALE)
-  .argument("<path>", "Target path")
+  .argument("[path]", "Target path", null, "content.json")
   .action(download)
 
   .command("update", "Update specified keys for given languages and save to target path")
@@ -24,13 +24,13 @@ cli
   .option("--default-locale <value>", "Default locale code to which empty keys should fall back to", null, process.env.PHRASEAPP_DEFAULT_LOCALE)
   .argument('<keys>', 'PhraseApp keys to update')
   .argument('<langs>', 'Languages')
-  .argument('<path>', 'Target path')
+  .argument('[path]', 'Target path', null, "content.json")
   .action(update)
 
   .command("upload ", "Upload specified content file to PhraseApp")
   .option("--project-id <value>", "PhraseApp project ID", null, process.env.PHRASEAPP_PROJECT_ID)
   .option("--access-token <value>", "PhraseApp access token", null, process.env.PHRASEAPP_ACCESS_TOKEN)
-  .argument('<path>', 'Source path')
+  .argument('[path]', 'Source path', null, "content.json")
   .action(upload)
 
 cli.parse(process.argv);
